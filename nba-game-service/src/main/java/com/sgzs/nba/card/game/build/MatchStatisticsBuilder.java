1c036e2ee16617ab473229e65a72426a7681c5ad
Jyf010205 <785923055@qq.com>
2021-11-10 23:45:37 +0800

    单元测试完成 比赛启动/跳球监听器

--- nba-game-service/src/main/java/com/sgzs/nba/card/game/build/MatchStatisticsBuilder.java
+++ nba-game-service/src/main/java/com/sgzs/nba/card/game/build/MatchStatisticsBuilder.java
@@ -16,15 +16,19 @@ public class MatchStatisticsBuilder {
         matchStatistics.setOffensiveRebounds(0);
         matchStatistics.setDefensiveRebounds(0);
         matchStatistics.setTotalRebounds(0);
+        matchStatistics.setAssist(0);
         matchStatistics.setSteals(0);
         matchStatistics.setBlocks(0);
+        matchStatistics.setPersonalFouls(0);
         matchStatistics.setTurnovers(0);
         matchStatistics.setFieldGoalAttempts(0);
+        matchStatistics.setFieldGoals(0);
         matchStatistics.setFreeThrowGoals(0);
         matchStatistics.setThreePointFieldGoalAttempts(0);
         matchStatistics.setThreePointFieldGoals(0);
         matchStatistics.setFreeThrowGoalAttempts(0);
         matchStatistics.setFreeThrowGoals(0);
+        matchStatistics.setPlusMinus(0);
         return matchStatistics;
     }
 
